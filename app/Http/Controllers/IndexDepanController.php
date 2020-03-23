876dc6884f90a1a4ea561b89baf5fe23aa827774
Alvin13 <alvin.anik13@gmail.com>
2020-03-23 13:28:07 +0700

    Frontend

--- app/Http/Controllers/IndexDepanController.php
+++ app/Http/Controllers/IndexDepanController.php
@@ -1,41 +1,21 @@
 <?php
-
-
-
 namespace App\Http\Controllers;
 
-
-
 use Illuminate\Http\Request;
-
 use App\Alamat;
-
 use App\Mainmenu;
-
 use App\Post;
-
 use App\Info;
-
 use App\Sosmed;
-
 use App\Map;
-
 use App\Slider;
-
 use App\M_Galeri;
-
 use App\M_Opini;
-
 use App\M_Resensi;
-
 use App\M_Youtube;
 
-
-
 class IndexDepanController extends Controller
-
 {
-
     /**
 
      * Display a listing of the resource.
@@ -47,37 +27,22 @@ class IndexDepanController extends Controller
      */
 
     public function index()
-
     {
+        // $almt = Alamat::findOrFail(1);
+        // $menus = Mainmenu::with('children')->where('parent','=',0)->orderBy('urutan')->get();
+        // $posts1 = Post::where('status', 'publish')->orderBy('tgl_buat','desc')->skip(0)->take(3)->get();
+        // $posts2 = Post::where('status', 'publish')->orderBy('tgl_buat','desc')->skip(3)->take(3)->get();
+        // $infos1 = Info::where('status', 'publish')->orderBy('tgl_buat','desc')->skip(0)->take(3)->get();
+        // $infos2 = Info::where('status', 'publish')->orderBy('tgl_buat','desc')->skip(3)->take(3)->get();        
 
-        $almt = Alamat::findOrFail(1);
+        // $sosmeds = Sosmed::all();
+        // $slides = Slider::orderBy('id','desc')->get();
+        // $images = M_Galeri::where('status', 'publish')->orderBy('tgl_buat','desc')->skip(0)->take(6)->get();
+        // $opini = M_Opini::where('status', 'publish')->orderBy('tgl_buat','desc')->take(8)->get();
+        // $resensi = M_Resensi::where('status', 'publish')->orderBy('tgl_buat','desc')->skip(0)->take(3)->get();
+        // $video = M_Youtube::where('status', 'publish')->orderBy('created_at','desc')->skip(0)->take(2)->get();
 
-        $menus = Mainmenu::with('children')->where('parent','=',0)->orderBy('urutan')->get();
-
-        $posts1 = Post::where('status', 'publish')->orderBy('tgl_buat','desc')->skip(0)->take(3)->get();
-
-        $posts2 = Post::where('status', 'publish')->orderBy('tgl_buat','desc')->skip(3)->take(3)->get();
-
-        $infos1 = Info::where('status', 'publish')->orderBy('tgl_buat','desc')->skip(0)->take(3)->get();
-
-        $infos2 = Info::where('status', 'publish')->orderBy('tgl_buat','desc')->skip(3)->take(3)->get();
-
-        
-
-        $sosmeds = Sosmed::all();
-
-        $slides = Slider::orderBy('id','desc')->get();
-
-        $images = M_Galeri::where('status', 'publish')->orderBy('tgl_buat','desc')->skip(0)->take(6)->get();
-
-        $opini = M_Opini::where('status', 'publish')->orderBy('tgl_buat','desc')->take(8)->get();
-        $resensi = M_Resensi::where('status', 'publish')->orderBy('tgl_buat','desc')->skip(0)->take(3)->get();
-
-        $video = M_Youtube::where('status', 'publish')->orderBy('created_at','desc')->skip(0)->take(2)->get();
-
-        return view('depan.depan', compact('almt','menus','opini','posts1','posts2','resensi','infos1','infos2','sosmeds','map','slides','images', 'video'));
-
+        return view('depan.depan');
     }
-
 }
 
--- resources/views/depan/bagian/meta.blade.php
+++ resources/views/depan/bagian/meta.blade.php
@@ -1,16 +1,32 @@
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<title>Covid-19 || Pemerintah Kabupaten Situbondo</title>
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
 
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="robots" content="index, follow">
-<meta name="keywords" content="kpu, kabupaten, situbondo">
-<meta name="description" content="Website KPU Kabupaten Situbondo">
-<meta name="author" content="Dinas Komunikasi Informatika dan Persandian Kabupaten Situbondo">
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-155879980-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+<!-- Favicon -->
+<link rel="shortcut icon" type="image/x-icon" href="{{asset('tema/kominfo_rsud_besuki/favicon.png')}}">
 
-  gtag('config', 'UA-155879980-1');
-</script>
+<!-- All css files are included here. -->
+<!-- Bootstrap fremwork main css -->
+<link rel="stylesheet" href="{{asset('/tema/frontend/css/bootstrap.min.css')}}">
+<!-- nivo slider CSS -->
+<link rel="stylesheet" href="{{asset('/tema/frontend/lib/css/nivo-slider.css')}}" />
+<!-- This core.css file contents all plugings css file. -->
+<link rel="stylesheet" href="{{asset('/tema/frontend/css/core.css')}}">
+<!-- Theme shortcodes/elements style -->
+<link rel="stylesheet" href="{{asset('/tema/frontend/css/shortcode/shortcodes.css')}}">
+<!-- Theme main style -->
+<link rel="stylesheet" href="{{asset('/tema/frontend/style.css')}}">
+<!-- Responsive css -->
+<link rel="stylesheet" href="{{asset('/tema/frontend/css/responsive.css')}}">
+<!-- User style -->
+<link rel="stylesheet" href="{{asset('/tema/frontend/css/custom.css')}}">
+
+<!-- Style customizer (Remove these two lines please) -->
+<link rel="stylesheet" href="{{asset('/tema/frontend/css/style-customizer.css')}}">
+<link href="#" data-style="styles" rel="stylesheet">
+
+
+<!-- Modernizr JS -->
+<script src="{{asset('/tema/frontend/js/vendor/modernizr-2.8.3.min.js')}}"></script>
